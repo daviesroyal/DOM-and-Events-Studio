2faa5e8b191109f1cda02cf6cbb757814baeedfa
Rhiannon Davies <ro.morningstar@gmail.com>
2020-10-14 18:36:14 -0500

    fixed the up and down buttons

--- scripts.js
+++ scripts.js
@@ -1,48 +1,61 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 window.addEventListener('load', function(){
+
     let takeoffButton = document.getElementById('takeoff');
     let landButton = document.getElementById('landing');
     let abortButton = document.getElementById('missionAbort');
     let flightStatus = document.getElementById('flightStatus');
-    let rightButton = document.getElementsByTagName('button').innerHTML('Right');
-    let leftButton = document.getElementsByTagName('button').innerHTML('Left');
-    let upButton = document.getElementsByTagName('button').innerHTML('Up');
-    let downButton = document.getElementsByTagName('button').innerHTML('Down');
-    let shuttleBackground = document.getElementById('shuttleBackground').style.backgroundColor;
-    let shuttleHeight = document.getElementById('spaceShuttleHeight').innerHTML;
+
     takeoffButton.addEventListener('click', function(){
         let takeoffConfirmation = window.confirm('Confirm that the shuttle is ready for takeoff.');
         if (takeoffConfirmation === true) {
             flightStatus.innerHTML = 'Shuttle in flight.';
-            shuttleBackground = 'blue';
-            shuttleHeight = 10000;
+            document.getElementById('shuttleBackground').style.backgroundColor = 'blue';
+            document.getElementById('spaceShuttleHeight').innerHTML = 10000;
         }
     });
+
     landButton.addEventListener('click', function(){
         window.alert('The shuttle is landing. Landing gear engaged.');
         flightStatus.innerHTML = 'Shuttle has landed.';
-        shuttleBackground = 'green';
-        shuttleHeight = 0;
+        document.getElementById('shuttleBackground').style.backgroundColor = 'green';
+        document.getElementById('spaceShuttleHeight').innerHTML = 0;
     });
+
     abortButton.addEventListener('click', function(){
         let abortConfirmation = window.confirm('Confirm that you want to abort the mission.');
         if (abortConfirmation === true) {
             flightStatus.innerHTML = 'Mission aborted.';
-            shuttleBackground = 'green';
-            shuttleHeight = 0;
+            document.getElementById('shuttleBackground').style.backgroundColor = 'green';
+            document.getElementById('spaceShuttleHeight').innerHTML = 0;
         }
     });
-    upButton.addEventListener('click', function(){
-        shuttleHeight += 10000;
-    });
-    downButton.addEventListener('click', function(){
-        shuttleHeight -= 10000;
-    });
-    rightButton.addEventListener('click', function(){
-        
-    });
-    leftButton.addEventListener('click', function(){
 
-    });
+    let directionButtons = document.getElementsByTagName('button');
+
+    for (let i = 0; i < directionButtons.length; i++) {
+        if (directionButtons[i].innerHTML === 'Right') {
+            directionButtons[i].addEventListener('click', function(){
+                document.getElementById('rocket');
+            });
+        } else if (directionButtons[i].innerHTML === 'Left') {
+            directionButtons[i].addEventListener('click', function(){
+                document.getElementById('rocket');
+            });
+        } else if (directionButtons[i].innerHTML === 'Up') {
+            directionButtons[i].addEventListener('click', function(){
+                let shuttleHeightUp = Number(document.getElementById('spaceShuttleHeight').innerHTML);
+                shuttleHeightUp += 10000;
+                document.getElementById('spaceShuttleHeight').innerHTML = shuttleHeightUp;
+            });
+        } else if (directionButtons[i].innerHTML === 'Down') {
+            directionButtons[i].addEventListener('click', function(){
+                let shuttleHeightDown = Number(document.getElementById('spaceShuttleHeight').innerHTML);
+                shuttleHeightDown -= 10000;
+                document.getElementById('spaceShuttleHeight').innerHTML = shuttleHeightDown;
+            });
+        }
+    }
+    
 });
