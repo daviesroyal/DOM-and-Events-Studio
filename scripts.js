4be7b99481d235cd561f1754aea3d155ca58b356
Rhiannon Davies <ro.morningstar@gmail.com>
2020-11-04 19:58:58 -0600

    done

--- scripts.js
+++ scripts.js
@@ -6,29 +6,33 @@ window.addEventListener('load', function(){
     let landButton = document.getElementById('landing');
     let abortButton = document.getElementById('missionAbort');
     let flightStatus = document.getElementById('flightStatus');
+    let backgroundColor = document.getElementById('shuttleBackground');
+    let shuttleHeight = document.getElementById('spaceShuttleHeight');
 
     takeoffButton.addEventListener('click', function(){
         let takeoffConfirmation = window.confirm('Confirm that the shuttle is ready for takeoff.');
         if (takeoffConfirmation === true) {
             flightStatus.innerHTML = 'Shuttle in flight.';
-            document.getElementById('shuttleBackground').style.backgroundColor = 'blue';
-            document.getElementById('spaceShuttleHeight').innerHTML = 10000;
+            backgroundColor.style.backgroundColor = 'blue';
+            shuttleHeight.innerHTML = 10000;
         }
     });
 
     landButton.addEventListener('click', function(){
         window.alert('The shuttle is landing. Landing gear engaged.');
         flightStatus.innerHTML = 'Shuttle has landed.';
-        document.getElementById('shuttleBackground').style.backgroundColor = 'green';
-        document.getElementById('spaceShuttleHeight').innerHTML = 0;
+        backgroundColor.style.backgroundColor = 'green';
+        shuttleHeight.innerHTML = 0;
+        document.getElementById('rocket').style.transform = 'initial';
     });
 
     abortButton.addEventListener('click', function(){
         let abortConfirmation = window.confirm('Confirm that you want to abort the mission.');
         if (abortConfirmation === true) {
             flightStatus.innerHTML = 'Mission aborted.';
-            document.getElementById('shuttleBackground').style.backgroundColor = 'green';
-            document.getElementById('spaceShuttleHeight').innerHTML = 0;
+            backgroundColor.style.backgroundColor = 'green';
+            shuttleHeight.innerHTML = 0;
+            document.getElementById('rocket').style.transform = 'initial';
         }
     });
 
@@ -37,23 +41,23 @@ window.addEventListener('load', function(){
     for (let i = 0; i < directionButtons.length; i++) {
         if (directionButtons[i].innerHTML === 'Right') {
             directionButtons[i].addEventListener('click', function(){
-                document.getElementById('rocket');
+                document.getElementById('rocket').style.transform = 'translateX(10px)';
             });
         } else if (directionButtons[i].innerHTML === 'Left') {
             directionButtons[i].addEventListener('click', function(){
-                document.getElementById('rocket');
+                document.getElementById('rocket').style.transform = 'translateX(-10px)';
             });
         } else if (directionButtons[i].innerHTML === 'Up') {
             directionButtons[i].addEventListener('click', function(){
-                let shuttleHeightUp = Number(document.getElementById('spaceShuttleHeight').innerHTML);
+                let shuttleHeightUp = Number(shuttleHeight.innerHTML);
                 shuttleHeightUp += 10000;
-                document.getElementById('spaceShuttleHeight').innerHTML = shuttleHeightUp;
+                shuttleHeight.innerHTML = shuttleHeightUp;
             });
         } else if (directionButtons[i].innerHTML === 'Down') {
             directionButtons[i].addEventListener('click', function(){
-                let shuttleHeightDown = Number(document.getElementById('spaceShuttleHeight').innerHTML);
+                let shuttleHeightDown = Number(shuttleHeight.innerHTML);
                 shuttleHeightDown -= 10000;
-                document.getElementById('spaceShuttleHeight').innerHTML = shuttleHeightDown;
+                shuttleHeight.innerHTML = shuttleHeightDown;
             });
         }
     }
